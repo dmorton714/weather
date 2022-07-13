6b29bdbdb99c2d0ef21fa1cdaddac1add1a1ff14
Danny Morton <102087408+dmorton714@users.noreply.github.com>
2022-07-12 21:57:55 -0400

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+from ast import Str
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -5,9 +6,48 @@ import datetime
 import csv
 import numpy as np
 
+filename = 'data.csv'
+with open('data.csv') as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+#print(header_row)
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
+
+#gets high temps 
+
+highs = [ ]
+for row in reader: 
+    high = int(row[5])
+    highs.append(high)
+
+# plot temps 
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c="red")
+
+#formatting
+ax.set_title( "highs", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("temo (F)", fontsize=16)
+ax.tick_params(axis= 'both', which='major', labelsize=16)
+
+plt.show()
+
+
+
+
+
+
+
+'''
 df = pd.read_csv (r'data.csv')
 
 df.dtypes
+
+#df = pd.DataFrame(columns= ['Date','Avg'])
+
 
 pd.to_datetime(df['Date'])
 
@@ -18,9 +58,11 @@ df['Date'].dt.month
 df['Date'].dt.year % 10
 #df.plot(x="Date",y="Avg", kind="scatter")
 
-print(df.head(20))
 
 
+print(df.head(10))
+
+'''
 
 # x label to be temps 
 # y label to be the month 
